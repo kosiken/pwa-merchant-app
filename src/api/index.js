42658bfd5c4bef50c252d5a9e0fabd649408bdd9
kosiken <Allisonkosy@gmail.com>
2020-08-10 12:29:46 +0000

    add sign up and sign in

---
+++ src/api/index.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+
+function goodResponse(resp) {
+
+  let val = resp.status === 200 || resp.status === 201;
+  return val;
+}
+
+class FiveChowError extends Error {
+  constructor(resp) {
+    super();
+    this.data = resp.data;
+    this.status = resp.status;
+  }
+}
+
+const Axios = axios.create({
+  baseURL:
+    (prod ? "https://api.500chow.com" : "http://127.0.0.1:8000") + "/api/",
+  headers: { "Content-Type": "application/json" },
+});
+
+class FiveApi {
+  /**
+   *
+   * @param {AxiosInstance} api
+   */
+  constructor(api) {
+    this.api = api;
+  }
+
+  /**
+   *
+   * @param {{username: string, password: string}} user
+   */
+  async login(user) {
+    try {
+      let resp = await this.api.post("/vendors/login/", user);
+
+      if (goodResponse(resp)) {
+        return resp.data;
+      } else {
+        throw new FiveChowError(resp);
+      }
+    } catch (err) {
+        throw new FiveChowError({
+            data: {
+                error: err.message,
+                code: 5010
+            }
+        })
+    }
+  }
+
+  async register (user) {
+    try {
+      let resp = await this.api.post("/vendors/login/", user);
+
+      if (goodResponse(resp)) {
+        return resp.data;
+      } else {
+        throw new FiveChowError(resp);
+      }
+    } catch (err) {
+        throw new FiveChowError({
+            data: {
+                error: err.message,
+                code: 5010
+            }
+        })
+    }    
+  }
+}
+
+
+
+export default new FiveApi(Axios);
