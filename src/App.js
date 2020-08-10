d73945aea83e425f6ce742295de58b9a9c8714aa
kosiken <Allisonkosy@gmail.com>
2020-08-10 13:08:24 +0000

    add state management

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { BottomNav } from './components'
+import { BottomNav } from "./components";
 import Customers from "./pages/Customers";
 import CreateOrder from "./pages/CreateOrder";
-import OrderPage from "./pages/OrderPage"
+import OrderPage from "./pages/OrderPage";
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <CreateOrder />
-        </Route>
-        <Route exact path="/customers">
-          <Customers />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        
-        <Route exact path="/notifications">
-          <OrderPage />
-        </Route>
-      </Switch>
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import fun from "./store";
 
-      <BottomNav />
-    </Router>
+function App() {
+  const { store, persistor } = fun();
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<p>loading</p>} persistor={persistor}>
+        <div>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <CreateOrder />
+              </Route>
+              <Route exact path="/customers">
+                <Customers />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+
+              <Route exact path="/notifications">
+                <OrderPage />
+              </Route>
+            </Switch>
+
+            <BottomNav />
+          </Router>
+        </div>
+      </PersistGate>
+    </Provider>
   );
 }
 
