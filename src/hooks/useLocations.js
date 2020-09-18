f6f8565ee21582f8a9db848d2fe6d05bc5cb07f4
kosiken <allisonkosy@gmail.com>
2020-09-18 12:02:16 +0100

    Refactor ui

---
+++ src/hooks/useLocations.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const useLocations = (value) => {
+  const [locations, setLocations] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  useEffect(() => {
+    if (value) {
+      setIsSearching(true);
+      try {
+        if (!window.FiveService) {
+          setIsSearching(false);
+          setHasError(true);
+          return;
+        }
+        setHasError(false);
+        window.FiveService.getPlacePredictions(
+          { input: value, bounds: window.FiveBounds },
+          (predictions, status) => {
+            console.log(predictions);
+            console.log(status);
+            if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+              if (predictions instanceof Array) setLocations(predictions);
+            }
+            setIsSearching(false);
+          }
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [value]);
+
+  return { locations, isSearching, hasError };
+};
+export default useLocations;
